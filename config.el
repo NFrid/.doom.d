9354c1b44a45128e3752bb510fb244365e7a0675
Nick Friday <nfriday@yandex.ru>
2021-09-16 00:31:45 +0300

    e

--- config.el
+++ config.el
@@ -1,19 +1,24 @@
 ;;; config.el -*- lexical-binding: t; -*-
 
+(defun my/git-ls ()
+  "Find file in the current Git repository."
+  (interactive)
+  (let* ((default-directory (locate-dominating-file
+                             default-directory ".git"))
+         (cands (split-string
+                 (shell-command-to-string
+                  "git ls-files --full-name --")
+                 "\n"))
+         (file (completing-read "Find file: " cands)))
+    (when file
+      (find-file file))))
+
 (setq user-full-name "Nick Friday"
       user-mail-address "nfriday@ya.ru")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Iosevka nf" :size 18)
+(defun my-font (size) (font-spec :family "Iosevka nf" :size size))
+(setq doom-font (my-font 18)
+      doom-big-font (my-font 32)
       doom-variable-pitch-font (font-spec :family "Arial" :size 20))
 
 (setq doom-theme 'doom-dracula)
@@ -22,14 +27,44 @@
 
 (setq display-line-numbers-type t)
 
-(setq tab-width 2)
-(setq standard-indent 2)
-(setq evil-shift-width 2)
+(setq-default tab-width 2)
+(setq-default standard-indent 2)
+(setq-default evil-shift-width 2)
 
 (setq calendar-week-start-day 1)
 
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
+(defun my/prettify/js-hook ()
+    (interactive)
+  (setq prettify-symbols-alist '()))
+(add-hook 'js-mode-hook 'my/prettify/js-hook)
+
+(defun my/prettify/org-hook ()
+    (interactive)
+    (setq prettify-symbols-alist '(
+                                   ("#+begin_src" . "")
+                                   ("#+end_src" . "―")
+                                   ("#+BEGIN_SRC" . "")
+                                   ("#+END_SRC" . "―")
+                                   ("#+begin_quote" . "")
+                                   ("#+end_quote" . "")
+                                   ("#+BEGIN_QUOTE" . "")
+                                   ("#+END_QUOTE" . "")
+                                   (":PROPERTIES:" . "")
+                                   (":END:" . "―")
+                                   ("#+STARTUP:" . "")
+                                   ("#+TITLE:" . "")
+                                   ("#+RESULTS:" . "")
+                                   ("#+NAME:" . "")
+                                   ("#+ROAM_TAGS:" . "")
+                                   ("#+FILETAGS:" . "")
+                                   ("#+HTML_HEAD:" . "")
+                                   ("#+SUBTITLE:" . "")
+                                   ("#+AUTHOR:" . "")
+                                   ("SCHEDULED:" . "")
+                                   ("DEADLINE:" . "")))
+    (prettify-symbols-mode 1))
+;; (add-hook 'org-mode-hook 'my/prettify/org-hook)
+
 ;; - `load!' for loading external *.el files relative to this one
 ;; - `use-package!' for configuring packages
 ;; - `after!' for running code after a package has loaded
@@ -37,14 +72,11 @@
 ;;   this file. Emacs searches the `load-path' when you load packages with
 ;;   `require' or `use-package'.
 ;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
 
+(use-package! org-roam
+  :bind
+  ("C-c i" . org-roam-node-insert)
+)
 
 (use-package! tree-sitter
   :config
@@ -52,70 +84,78 @@
   (global-tree-sitter-mode)
   (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
   (pushnew! tree-sitter-major-mode-language-alist
-            '(scss-mode . css))
-  )
+            '(scss-mode . css)))
 
 (use-package! rainbow-delimiters
   :config
-  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
-  )
+  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
 
 (use-package! highlight-indent-guides
   :config
-  (add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
-  (setq highlight-indent-guides-responsive 'stack)
-  )
+  (add-hook 'prog-mode-hook 'highlight-indent-guides-mode))
 
 (use-package! company
   :config
   (setq company-show-quick-access t)
-  (define-key company-active-map (kbd "C-SPC") #'company-abort)
-  )
+  (define-key company-active-map (kbd "C-SPC") #'company-abort))
 
 (use-package! company-tabnine
   :config
-  (add-to-list company-backends #'company-tabnine)
-  )
+  (add-to-list 'company-backends #'company-tabnine))
 
 (custom-set-faces
  '(org-level-1 ((t (:inherit outline-1 :height 1.5))))
  '(org-level-2 ((t (:inherit outline-2 :height 1.4))))
  '(org-level-3 ((t (:inherit outline-3 :height 1.3))))
  '(org-level-4 ((t (:inherit outline-4 :height 1.2))))
- '(org-level-5 ((t (:inherit outline-5 :height 1.1))))
- )
+ '(org-level-5 ((t (:inherit outline-5 :height 1.1)))))
 
 (use-package! reverse-im
   :config
   (setq reverse-im-input-methods '("russian-computer"))
-  (reverse-im-mode)
-  )
+  (reverse-im-mode))
 
 ;; mappings
 
+(map! :n "C-h" '+tabs:previous-or-goto
+      :n "C-l" '+tabs:next-or-goto)
+
+(defun my/c-mode-common-hook ()
+  (interactive)
+  (map! :map (c-mode-map c++-mode-map cpp-mode-map)
+        :n "C-h" '+tabs:previous-or-goto
+        :n "C-l" '+tabs:next-or-goto))
+(add-hook 'c-mode-common-hook 'my/c-mode-common-hook)
+
+(defun my/the/delag ()
+  (interactive)
+  (so-long-minor-mode 1)
+  (rainbow-delimiters-mode 1)
+  (flycheck-mode 1)
+  (hl-line-mode 1)
+  (highlight-indent-guides-mode 1)
+  (smartparens-mode 1)
+)
+
+(map! :leader :desc "Delag current buffer"
+      :n "zd" 'my/the/delag)
+
+(map! :leader :desc "Find in git ls-files"
+      :n "fg" 'my/git-ls)
+
 (map!
  :i "C-h" 'backward-delete-char
- (:mode org-mode
+ (:after org :map org-mode-map
   :i "C-h" 'backward-delete-char
-  :i "C-l" 'recenter-top-bottom
-  )
- )
+  :i "C-l" 'recenter-top-bottom))
 
-(map! :n "C-h" '+tabs:previous-or-goto
-      :n "C-l" '+tabs:next-or-goto
-      :n "SPC ESC" 'centaur-tabs-toggle-groups
-      (:mode (c-mode c++-mode cpp-mode)
-       :n "C-h" '+tabs:previous-or-goto
-       :n "C-l" '+tabs:next-or-goto
-       )
-      )
+(map! :n "SPC ESC" 'centaur-tabs-toggle-groups)
 
 (map! :n "Q" 'kill-this-buffer)
 
 (map! :leader
       :desc "Next workspace" :n "j" '+workspace:switch-next
-      :desc "Prev workspace" :n "k" '+workspace:switch-previous
-      )
+      :desc "Prev workspace" :n "k" '+workspace:switch-previous)
 
 (map! :n "s" nil)
 (map! :prefix "s"
@@ -139,5 +179,4 @@
       :desc "find char" :mn "f" 'evilem-motion-find-char
       :desc "find char to" :mn "t" 'evilem-motion-find-char-to
       :desc "find char back" :mn "F" 'evilem-motion-find-char-backward
-      :desc "find char back to" :mn "T" 'evilem-motion-find-char-backward-to
-      )
+      :desc "find char back to" :mn "T" 'evilem-motion-find-char-backward-to)
